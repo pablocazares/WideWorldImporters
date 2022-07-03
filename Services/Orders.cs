72486b7f3ae9f4dcf611ded196c4d2dfbea20599
pablocazares <pablocazares@xentrina.com>
2022-07-03 13:53:22 -0700

    adding data projects
    adding data projects

---
+++ Services/Orders.cs
@@ -0,0 +1,16 @@
+﻿using Repositories;
+
+namespace Services
+{
+    public class Orders
+    {
+        public void getAllOrders()
+        {
+            var wwiContext = new WideWorldImportersContext();
+            var orders = wwiContext.PurchaseOrders;
+
+        }
+        
+
+    }
+}
